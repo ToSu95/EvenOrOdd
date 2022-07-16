dff7a20168a33b67609e9ddb6bed24e039ff2f76
ToSu95 <t.suska95@gmail.com>
2022-07-16 15:31:26 +0200

    Add project files.

---
+++ EvenorOdd/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenorOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę, a ja sprawdzę czy jest parzysta czy nieparzysta.");
+
+            while(true)
+            { 
+            Console.WriteLine("Podaj liczbę:");
+            var userNumber = GetNumber();
+
+            if (userNumber % 2 == 0)
+                Console.WriteLine("Liczba jest parzysta.");
+            else
+                Console.WriteLine("Liczba jest nieparzysta.");
+
+            Console.WriteLine("Jeśli chcesz sprawdzić kolejne liczby to podaj liczbę, a jeśli chcesz wyjść z aplikacji wpisz 'x'.");
+            }
+        }
+
+        private static int GetNumber()
+        {
+            while(true)
+            {
+                var userChoice = Console.ReadLine();
+
+                if(userChoice.ToUpper() == "X")
+                {
+                    Environment.Exit(0);
+                }
+
+                if(!int.TryParse(userChoice, out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłowe dane! Spróbuj ponownie.");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
